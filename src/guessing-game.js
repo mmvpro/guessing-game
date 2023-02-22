3e21396b27040a94f04be29788d1004cb5c6584f
mmvpro <dublibum@gmail.com>
2023-02-23 00:55:53 +0300

    done GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.gues = 0;
     }
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.g=0; 
+    }
+    
     guess() {
+        this.g =  Math.round((this.max + this.min) / 2) ;
+        return this.g
 
     }
 
     lower() {
+        this.max = this.g
 
     }
 
     greater() {
+        this.min = this.g
 
     }
 }
